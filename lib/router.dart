7989d686f6ae24899b0362a99cd2808dcd316afa
nikarora1111 <nikarora1111@gmail.com>
2020-03-25 17:47:28 +0530

    Few modification to add app launching method

--- lib/router.dart
+++ lib/router.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'pay_method.dart';
 import 'home_page.dart';
-
+import 'temp_pay_method.dart';
 const String splashScreenViewRoute = '/';
 const String homeRoute = 'home';
 const String payRoute = 'pay';
@@ -11,6 +11,34 @@ Route<dynamic> generateRoute(RouteSettings settings) {
     case homeRoute:
       return MaterialPageRoute(builder: (context) => HomePage());
     case payRoute:
-      return MaterialPageRoute(builder: (context) => PayMethod());
+      return MaterialPageRoute(builder: (context) => TempPayMethod());
   }
 }
+
+/*List<Map<String, String>> appList=[
+    {
+      'app_name':'PayTm',
+      'package_name':'net.one97.paytm'
+    },
+    {
+      'app_name':'GooglePay',
+      'package_name':'com.google.android.apps.nbu.paisa.user'
+    },
+    {
+      'app_name':'BhimUpi',
+      'package_name':'in.org.npci.upiapp'
+    },
+    {
+      'app_name':'PhonePe',
+      'package_name':'com.phonepe.app'
+    },
+    {
+      'app_name':'MiPay',
+      'package_name':'com.mipay.wallet.in'
+    },
+    {
+      'app_name':'AmaxonPay',
+      'package_name':'in.amazon.mShop.android.shopping'
+    },
+
+  ];*/
